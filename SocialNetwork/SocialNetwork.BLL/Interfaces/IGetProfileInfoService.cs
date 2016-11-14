2499deb0e26e2028a891e2e01b2a850d5f476863
Zheka <Zheka@LINKPC>
2016-11-14 19:46:06 +0300

    Some methods in GetProfileInfoService are added

---
+++ SocialNetwork/SocialNetwork.BLL/Interfaces/IGetProfileInfoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialNetwork.BLL.DTO;
+using SocialNetwork.DAL.EF;
+
+namespace SocialNetwork.BLL.Interfaces
+{
+   public interface IGetProfileInfoService
+    {
+        ProfileDTO GetProfileById(int id);
+        ProfileDTO GetProfileByIdentityName(string identityName);
+
+        ProfileDTO GetFollowers(int id);
+        ProfileDTO GetSubscriptions(int id);
+
+
+    }
+}
---
+++ SocialNetwork/SocialNetwork.BLL/Services/GetProfileInfoService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialNetwork.BLL.DTO;
+using SocialNetwork.BLL.Interfaces;
+using SocialNetwork.DAL.Repositories;
+using SocialNetwork.DAL.Interfaces;
+using AutoMapper;
+namespace SocialNetwork.BLL.Services
+{
+   public class GetProfileInfoService : IGetProfileInfoService
+    {
+        private IUnitOfWork uow;
+
+        public GetProfileInfoService(IUnitOfWork uow) {
+            this.uow = uow;
+        }
+
+        public ProfileDTO GetProfileById(int id) {
+            Mapper.Initialize(cfg => cfg.CreateMap<SocialNetwork.DAL.EF.Profile, ProfileDTO>());
+            return Mapper.Map<ProfileDTO>(uow.Profiles.Get(id));
+
+        }
+        public ProfileDTO GetProfileByIdentityName(string identityName) {
+            Mapper.Initialize(cfg => cfg.CreateMap<SocialNetwork.DAL.EF.Profile, ProfileDTO>());
+            return Mapper.Map<ProfileDTO>(uow.Profiles.FindByIdentityName(identityName));
+        }
+
+
+        public ICollection<ProfileDTO> GetFollowers(int id) {
+            SocialNetwork.DAL.EF.Profile profile = uow.Profiles.Get(id);
+            if (profile != null) {
+                ICollection<SocialNetwork.DAL.EF.Profile> followers = profile.Followers;
+                Mapper.Initialize(cfg => cfg.CreateMap<SocialNetwork.DAL.EF.Profile, ProfileDTO>());
+                return Mapper.Map<ICollection<SocialNetwork.DAL.EF.Profile>, ICollection<ProfileDTO>>(followers);
+            }
+            return null;
+        }
+
+        public ICollection<ProfileDTO> GetSubscriptions(int id) {
+            SocialNetwork.DAL.EF.Profile profile = uow.Profiles.Get(id);
+            if (profile != null) {
+                ICollection<SocialNetwork.DAL.EF.Profile> subscriptions = profile.SubscribedOn;
+                Mapper.Initialize(cfg => cfg.CreateMap<SocialNetwork.DAL.EF.Profile,ProfileDTO>());
+                return Mapper.Map<ICollection<SocialNetwork.DAL.EF.Profile>, ICollection<ProfileDTO>>(subscriptions);
+            }
+            return null;
+        }
+
+
+
+
+    }
+}
