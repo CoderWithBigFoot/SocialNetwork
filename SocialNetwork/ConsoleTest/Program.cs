c2dd00d90435117bfaf2a54460d774f06b9dc417
Zheka <Zheka@LINKPC>
2016-11-14 17:04:44 +0300

    BLL was added

--- SocialNetwork/ConsoleTest/Program.cs
+++ SocialNetwork/ConsoleTest/Program.cs
@@ -162,27 +162,16 @@ namespace ConsoleTest
                     };
                     post1.Hashtags.Add(uow.Hashtags.Find(x => x.Name == "cat").FirstOrDefault());
                     uow.Posts.Create(post1);*/
-                using (SocialNetworkContext uow = new SocialNetworkContext()) {
-                    
-
-                    Profile prof1 = uow.Profiles.FirstOrDefault(x=>x.Name == "Zheka");
-                    Profile prof2 = uow.Profiles.FirstOrDefault(x => x.Name == "ser");
-                    Post post1 = uow.Posts.FirstOrDefault(x=>x.Content == "post1");
-
-                    //post1.Hashtags.Add(uow.Hashtags.FirstOrDefault(x=>x.Name=="cat"));
-
-
-                   // Console.WriteLine(uow.SaveChanges());
-
-
-
-                }
+            
                 using (UnitOfWork uow = new UnitOfWork()) {
                     Profile prof1 = uow.Profiles.FindByIdentityName("prof1");
                     Profile prof2 = uow.Profiles.FindByIdentityName("prof2");
-                   
-                    Post post1 = uow.Posts.Find(x=>x.Content == "post1 changed content").FirstOrDefault();
-                    
+                    Post post1 = uow.Posts.Find(x=>x.Content == "post1").FirstOrDefault();
+
+
+                    post1.LikeVoices.Remove(prof2);
+
+
                     Console.WriteLine(uow.Save());
                 }
 
