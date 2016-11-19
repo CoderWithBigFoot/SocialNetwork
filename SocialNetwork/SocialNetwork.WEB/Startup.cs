fbad909ae77ad187da5c5a7045ec883e8f3dd6e8
Zheka <Zheka@LINKPC>
2016-11-19 20:30:09 +0300

    SocialNetwork.WEB is added

---
+++ SocialNetwork/SocialNetwork.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialNetwork.WEB.Startup))]
+namespace SocialNetwork.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
